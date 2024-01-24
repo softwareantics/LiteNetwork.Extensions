42cc811c8bca8202bc8b428ec241c355dbd5b648
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2024-01-24 20:10:56 +1100

    Updated assembly version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2023 Software Antics")]
 [assembly: AssemblyTrademark("Software Antics™")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.4.1.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.2.0")]
+[assembly: AssemblyFileVersion("2.4.2.0")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
